c1b3c4d909773af25c11920afc14bec600e94b58
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2025-09-14 21:53:34 +0900

    add: jest 알아보기중

--- next/law-project/fn.js
+++ next/law-project/fn.js
@@ -1,6 +1,9 @@
 const fn = {
     add: (num1, num2) => num1 + num2,
-    makeUser: (name, age) => ({ name, age }),
+    makeUser: (name, age) => ({ name, age, gender: undefined }),
+    thorwErr: () => {
+        throw new Error("XX");
+    },
 };
 
 module.exports = fn;
--- next/law-project/fn.test.js
+++ next/law-project/fn.test.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const fn = require("./fn");
 
 test("1은 1이다", () => {
@@ -10,4 +11,64 @@ test("2 더하기 3은 5야.", () => {
 
 test("3 더하기 3은 5가 아니야.", () => {
     expect(fn.add(3, 3)).not.toBe(5);
+});
+
+test("gender라는 것이 없어도 넘어가짐 이름과 나이를 전달받아서 객체를 반환해줘", () => {
+    expect(fn.makeUser("Mike", 30)).toEqual({
+        name: "Mike",
+        age: 30,
+    });
+});
+
+test("보다 엄격하게-이름과 나이를 전달받아서 객체를 반환해줘", () => {
+    expect(fn.makeUser("Mike", 30, undefined)).toStrictEqual({
+        name: "Mike",
+        age: 30,
+        gender: undefined,
+    });
+});
+
+test("null은 null입니다.", () => {
+    expect(null).toBeNull();
+});
+
+test("0은 false입니다.", () => {
+    expect(fn.add(1, -1)).toBeFalsy();
+});
+
+test("비어있지 않은 문자열을 true입니다.", () => {
+    expect(fn.add("Hello", "World")).toBeTruthy();
+});
+
+test("ID는 10자 이하여야 합니다.", () => {
+    // const id = "THE_BLACK_OREDER";
+    const id = "THE_BLACK";
+
+    expect(id.length).toBeLessThanOrEqual(10);
+});
+
+test("비밀번호 4자리", () => {
+    const pw = "1234";
+
+    expect(pw.length).toBe(4);
+    expect(pw.length).toEqual(4);
+});
+
+test("0.1더하기 0.2는 0.3입니다.", () => {
+    expect(fn.add(0.1, 0.2)).toBeCloseTo(0.3);
+});
+
+test("Hello World에 a라는 글자가 있나?", () => {
+    expect("Hello World").toMatch(/h/i);
+});
+
+test("유저 리스트에 Mike가 있나?", () => {
+    const user = "Mike";
+    const userList = ["Tom", "Jane", "Kai", "Mike"];
+
+    expect(userList).toContain(user);
+});
+
+test("이거 에러 나나요?", () => {
+    expect(() => fn.thorwErr()).toThrow("XX");
 });
